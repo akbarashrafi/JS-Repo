244eab94231f93a5afc35c3daeb36f93f4ae28d3
akbarashrafi <akbar.ahmed831@gmail.com>
2017-06-14 14:45:31 +0530

    updated

---
+++ buildScripts/distServer.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+/*eslint-disable no-console*/
+
+const port = 3000;
+const app = express();
+
+app.use(compression());
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.get('/users', function(req, res) {
+    //hard coding for simplicity. Pretend this hits a real database
+    res.json([
+        { "id": 1, "firstName": "shane", "lastName": "warne", "email": "waseem@yahoo.com" },
+        { "id": 2, "firstName": "Waseem", "lastName": "Akram", "email": "akram@gmail.com" },
+        { "id": 3, "firstName": "Bod", "lastName": "smith", "email": "wasee@hot.com" }
+    ]);
+});
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err)
+    } else {
+        open('http://localhost:' + port);
+    }
+});
